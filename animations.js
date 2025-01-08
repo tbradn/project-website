7aee30189414f233d0bb62fe56287b8ee82044cd
tbradn <87394641+tbradn@users.noreply.github.com>
2025-01-08 17:28:21 -0600

    Update animations.js

--- animations.js
+++ animations.js
@@ -31,7 +31,7 @@ sections.forEach(section => {
 // Header shrink effect on scroll
 const header = document.querySelector('header');
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 10) {
+    if (window.scrollY > 50) {
         header.classList.add('shrink');
     } else {
         header.classList.remove('shrink');
